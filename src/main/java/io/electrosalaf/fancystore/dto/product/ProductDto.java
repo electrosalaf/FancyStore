00c678b3db021af40aa9a1a2ccd1a35604c35341
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-08-24 02:37:49 +0100

    modeled product dto for data transfer between processes

---
+++ src/main/java/io/electrosalaf/fancystore/dto/product/ProductDto.java
@@ -0,0 +1,40 @@
+package io.electrosalaf.fancystore.dto.product;
+
+import io.electrosalaf.fancystore.model.Product;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProductDto {
+    private Integer id;
+    private @NotNull String name;
+    private @NotNull String imageURL;
+    private @NotNull double price;
+    private @NotNull String description;
+    private @NotNull Integer categoryId;
+
+    public ProductDto(@NonNull String name, @NonNull String imageURL,
+                      @NonNull double price, @NotNull String description,
+                      @NotNull Integer categoryId) {
+        this.name = name;
+        this.imageURL = imageURL;
+        this.price = price;
+        this.description = description;
+        this.categoryId = categoryId;
+    }
+
+    public ProductDto(Product product) {
+        this.setId(product.getId());
+        this.setName(product.getName());
+        this.setImageURL(product.getImageURL());
+        this.setDescription(product.getDescription());
+        this.setPrice(product.getPrice());
+        this.setCategoryId(product.getCategory().getId());
+    }
+}
