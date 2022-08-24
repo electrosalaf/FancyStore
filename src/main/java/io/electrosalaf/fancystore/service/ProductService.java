6f80b703f01f555edaaac7174e24afdc572e4786
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-08-24 11:08:18 +0100

    implemented service to list all products

--- src/main/java/io/electrosalaf/fancystore/service/ProductService.java
+++ src/main/java/io/electrosalaf/fancystore/service/ProductService.java
@@ -7,6 +7,9 @@ import io.electrosalaf.fancystore.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ProductService {
 
@@ -17,11 +20,6 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public void addProduct(ProductDto productDto, Category category) {
-        Product product = getProductFromDto(productDto, category);
-        productRepository.save(product);
-    }
-
     private Product getProductFromDto(ProductDto productDto, Category category) {
         Product product = new Product();
         product.setCategory(category);
@@ -30,5 +28,27 @@ public class ProductService {
         product.setPrice(productDto.getPrice());
         product.setDescription(productDto.getDescription());
         return product;
+    }
+
+    public void addProduct(ProductDto productDto, Category category) {
+        Product product = getProductFromDto(productDto, category);
+        productRepository.save(product);
+    }
+
+    public List<ProductDto> listProducts() {
+        List<Product> products = productRepository.findAll();
+        List<ProductDto> productDtos = new ArrayList<>();
+
+        for (Product product: products) {
+            productDtos.add(new ProductDto(product));
+        }
+        return productDtos;
+    }
+
+    public void updateProduct(Integer productID, ProductDto productDto, Category category) {
+        Product product = getProductFromDto(productDto, category);
+
+        product.setId(productID);
+        productRepository.save(product);
     }
 }
