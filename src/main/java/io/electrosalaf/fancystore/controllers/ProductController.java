3413dc35e5c4ee1d8e83aeae1afb18d2d2072b7b
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-08-24 03:58:43 +0100

    implemented add product API

--- src/main/java/io/electrosalaf/fancystore/controllers/ProductController.java
+++ src/main/java/io/electrosalaf/fancystore/controllers/ProductController.java
@@ -1,4 +1,43 @@
 package io.electrosalaf.fancystore.controllers;
 
+import io.electrosalaf.fancystore.config.ApiResponse;
+import io.electrosalaf.fancystore.dto.product.ProductDto;
+import io.electrosalaf.fancystore.model.Category;
+import io.electrosalaf.fancystore.service.CategoryService;
+import io.electrosalaf.fancystore.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/product")
 public class ProductController {
+
+    private final ProductService productService;
+    private final CategoryService categoryService;
+
+    @Autowired
+    public ProductController(ProductService productService, CategoryService categoryService) {
+        this.productService = productService;
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addProduct(@RequestBody ProductDto productDto) {
+        Optional<Category> optionalCategory = categoryService.readCategory(productDto.getCategoryId());
+        if (!optionalCategory.isPresent()) {
+            return new ResponseEntity<>(new ApiResponse(false, "category is invalid"), HttpStatus.CONFLICT);
+        }
+
+        Category category = optionalCategory.get();
+        productService.addProduct(productDto, category);
+
+        return new ResponseEntity<>(new ApiResponse(true, "Product added successfully"), HttpStatus.CREATED);
+    }
 }
