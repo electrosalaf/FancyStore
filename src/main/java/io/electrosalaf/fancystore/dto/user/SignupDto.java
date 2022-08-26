f035022819935715ce366e9f952e32decb34e702
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-08-26 23:08:13 +0100

    implemented signup dto

---
+++ src/main/java/io/electrosalaf/fancystore/dto/user/SignupDto.java
@@ -0,0 +1,13 @@
+package io.electrosalaf.fancystore.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignupDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
