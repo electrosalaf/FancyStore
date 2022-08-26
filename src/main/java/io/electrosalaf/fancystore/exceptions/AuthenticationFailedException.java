13e828c433a53b9dd716634474cf75212765d4e6
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-08-26 23:10:47 +0100

    added failed auth class

---
+++ src/main/java/io/electrosalaf/fancystore/exceptions/AuthenticationFailedException.java
@@ -0,0 +1,7 @@
+package io.electrosalaf.fancystore.exceptions;
+
+public class AuthenticationFailedException extends Exception {
+    public AuthenticationFailedException(String message) {
+        super(message);
+    }
+}
