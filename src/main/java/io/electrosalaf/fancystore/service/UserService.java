57cbfe020d42c8cfb67cd5e3ba90fbc949cb18ea
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-08-26 23:29:08 +0100

    implemented user service

---
+++ src/main/java/io/electrosalaf/fancystore/service/UserService.java
@@ -0,0 +1,61 @@
+package io.electrosalaf.fancystore.service;
+
+import io.electrosalaf.fancystore.dto.user.SignupDto;
+import io.electrosalaf.fancystore.dto.user.SignupResponseDto;
+import io.electrosalaf.fancystore.exceptions.CustomException;
+import io.electrosalaf.fancystore.model.User;
+import io.electrosalaf.fancystore.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public SignupResponseDto signup(SignupDto signupDto) throws CustomException {
+
+        if (Objects.nonNull(userRepository.findByEmail(signupDto.getEmail()))) {
+            throw new CustomException("User already exists");
+        }
+
+        String encryptedPassword = signupDto.getPassword();
+        try {
+            encryptedPassword = hashPassword(signupDto.getPassword());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            logger.error("hashing password failed {}", e.getMessage());
+        }
+
+        User user = new User(signupDto.getFirstName(), signupDto.getLastName(), signupDto.getEmail(), encryptedPassword);
+
+        try {
+            userRepository.save(user);
+            return new SignupResponseDto("success", "user created successfully");
+        } catch (Exception e) {
+            throw new CustomException(e.getMessage());
+        }
+    }
+
+    private String hashPassword(String password) throws NoSuchAlgorithmException {
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        md5.update(password.getBytes());
+        byte[] digest = md5.digest();
+        String hashes = DatatypeConverter.printHexBinary(digest).toUpperCase();
+        return hashes;
+    }
+}
