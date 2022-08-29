864e6c6cf978c1a012e7b8f22858404eb73475b7
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-08-29 07:48:27 +0100

    Fixed signin bug

--- src/main/java/io/electrosalaf/fancystore/service/UserService.java
+++ src/main/java/io/electrosalaf/fancystore/service/UserService.java
@@ -1,8 +1,13 @@
 package io.electrosalaf.fancystore.service;
 
-import io.electrosalaf.fancystore.dto.user.SignupDto;
+import io.electrosalaf.fancystore.config.MessageStrings;
+import io.electrosalaf.fancystore.dto.user.SignInDto;
+import io.electrosalaf.fancystore.dto.user.SignInResponseDto;
+import io.electrosalaf.fancystore.dto.user.SignUpDto;
 import io.electrosalaf.fancystore.dto.user.SignupResponseDto;
+import io.electrosalaf.fancystore.exceptions.AuthenticationFailException;
 import io.electrosalaf.fancystore.exceptions.CustomException;
+import io.electrosalaf.fancystore.model.AuthenticationToken;
 import io.electrosalaf.fancystore.model.User;
 import io.electrosalaf.fancystore.repository.UserRepository;
 import org.slf4j.Logger;
@@ -19,20 +24,25 @@ import java.util.Objects;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final AuthenticationService authenticationService;
 
     Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, AuthenticationService authenticationService) {
         this.userRepository = userRepository;
+        this.authenticationService = authenticationService;
     }
 
-    public SignupResponseDto signup(SignupDto signupDto) throws CustomException {
+    // Check if current email address is registered
+    public SignupResponseDto signup(SignUpDto signupDto) throws CustomException {
 
         if (Objects.nonNull(userRepository.findByEmail(signupDto.getEmail()))) {
+            // throw exception if registered
             throw new CustomException("User already exists");
         }
 
+        // encrypting the password
         String encryptedPassword = signupDto.getPassword();
         try {
             encryptedPassword = hashPassword(signupDto.getPassword());
@@ -45,17 +55,51 @@ public class UserService {
 
         try {
             userRepository.save(user);
+            // success in creating
+            // generate token for user
+            final AuthenticationToken authenticationToken = new AuthenticationToken(user);
+            // save token in database
+            authenticationService.saveConfirmationToken(authenticationToken);
             return new SignupResponseDto("success", "user created successfully");
         } catch (Exception e) {
+            // handle signup error
             throw new CustomException(e.getMessage());
         }
     }
 
-    private String hashPassword(String password) throws NoSuchAlgorithmException {
+    String hashPassword(String password) throws NoSuchAlgorithmException {
         MessageDigest md5 = MessageDigest.getInstance("MD5");
         md5.update(password.getBytes());
         byte[] digest = md5.digest();
         String hashes = DatatypeConverter.printHexBinary(digest).toUpperCase();
         return hashes;
+    }
+
+    public SignInResponseDto signIn(SignInDto signInDto) throws AuthenticationFailException, CustomException {
+        // find user by email
+        User user = userRepository.findByEmail(signInDto.getEmail());
+        if (!Objects.nonNull(user)) {
+            throw new AuthenticationFailException("User does not exist");
+        }
+
+        // check if the password is right
+        try {
+            if (!user.getPassword().equals(hashPassword(signInDto.getPassword()))) {
+                // password do not match
+                throw new AuthenticationFailException(MessageStrings.WRONG_PASSWORD);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            logger.error("hashing password failed {}", e.getMessage());
+            throw new CustomException(e.getMessage());
+        }
+
+        AuthenticationToken token = authenticationService.getToken(user);
+
+        if (!Objects.nonNull(token)) {
+            throw new CustomException(MessageStrings.AUTH_TOKEN_NOT_PRESENT);
+        }
+
+        return new SignInResponseDto("success", token.getToken());
     }
 }
