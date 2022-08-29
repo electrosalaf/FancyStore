0fcd064928746cabaf7972e1401d6f6d99de30e6
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-08-29 07:50:09 +0100

    Implemented repo to interact with DB

---
+++ src/main/java/io/electrosalaf/fancystore/repository/CartRepository.java
@@ -0,0 +1,10 @@
+package io.electrosalaf.fancystore.repository;
+
+import io.electrosalaf.fancystore.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Integer> {
+
+}
