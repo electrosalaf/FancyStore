f859370ce72950510eb7faf18f76fbbee6c128b8
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-08-29 07:52:54 +0100

    Added model for Product

--- src/main/java/io/electrosalaf/fancystore/model/Product.java
+++ src/main/java/io/electrosalaf/fancystore/model/Product.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -13,6 +14,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class Product {
 
     @Id
