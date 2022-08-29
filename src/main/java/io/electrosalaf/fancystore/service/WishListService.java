5ec0ee20b7f8740eade493a95fa16ffced1ff8e4
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-08-29 07:46:09 +0100

    Implemented WishList Service

---
+++ src/main/java/io/electrosalaf/fancystore/service/WishListService.java
@@ -0,0 +1,28 @@
+package io.electrosalaf.fancystore.service;
+
+import io.electrosalaf.fancystore.model.User;
+import io.electrosalaf.fancystore.model.WishList;
+import io.electrosalaf.fancystore.repository.WishListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WishListService {
+
+    private final WishListRepository wishListRepository;
+
+    @Autowired
+    public WishListService(WishListRepository wishListRepository) {
+        this.wishListRepository = wishListRepository;
+    }
+
+    public void createWishList(WishList wishList) {
+        wishListRepository.save(wishList);
+    }
+
+    public List<WishList> readWishList(User user) {
+        return wishListRepository.findAllByUserOrderByCreatedDateDesc(user);
+    }
+}
