01d4b64c2ff74aec1fcafd7dd311c0744c9d68ab
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-08-30 18:58:48 +0100

    fixed error with method name nonconformity with table name

--- src/main/java/io/electrosalaf/fancystore/repository/CartRepository.java
+++ src/main/java/io/electrosalaf/fancystore/repository/CartRepository.java
@@ -1,10 +1,13 @@
 package io.electrosalaf.fancystore.repository;
 
 import io.electrosalaf.fancystore.model.Cart;
+import io.electrosalaf.fancystore.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Integer> {
-
+    List<Cart> findAllByUserOrderByCreatedDateDesc(User user);
 }
