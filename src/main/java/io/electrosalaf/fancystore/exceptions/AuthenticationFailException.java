eb45bd39dfad8646b47b41ba44e1980e9649fa1f
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-09-02 23:53:22 +0100

    minor refactor

--- src/main/java/io/electrosalaf/fancystore/exceptions/AuthenticationFailException.java
+++ src/main/java/io/electrosalaf/fancystore/exceptions/AuthenticationFailException.java
@@ -1,7 +1,7 @@
 package io.electrosalaf.fancystore.exceptions;
 
 public class AuthenticationFailException extends Exception {
-    public AuthenticationFailException(String message) {
-        super(message);
+    public AuthenticationFailException(String msg) {
+        super(msg);
     }
 }
