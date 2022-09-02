84ceeecec068e165e49a7dcf70d56a251027af87
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-09-02 23:45:52 +0100

    modeled stripe response dto

---
+++ src/main/java/io/electrosalaf/fancystore/dto/checkout/StripeResponse.java
@@ -0,0 +1,12 @@
+package io.electrosalaf.fancystore.dto.checkout;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StripeResponse {
+
+    private String sessionId;
+}
