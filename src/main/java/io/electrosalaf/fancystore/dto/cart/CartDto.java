0df01c419f7c75aeb9cba526bb3ff9bb2e351070
electrosalaf <lawalibrahimayodeji@gmail.com>
2022-09-02 23:43:36 +0100

    refactored cartdto

---
+++ src/main/java/io/electrosalaf/fancystore/dto/cart/CartDto.java
@@ -0,0 +1,16 @@
+package io.electrosalaf.fancystore.dto.cart;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CartDto {
+    private List<CartItemDto> cartItems;
+    private double totalCost;
+
+}
